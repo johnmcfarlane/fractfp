da6600e2b019eb6d6a5e90d4108b53fcec8dda74
John McFarlane <fixed-point@john.mcfarlane.name>
2017-07-17 07:52:29 -0700

    various: - replace config macros flags with constexpr bools - print stats about output

--- main.cpp
+++ main.cpp
@@ -2,7 +2,40 @@
 
 #include <sg14/auxiliary/boost.simd.h>
 
-#define USE_FIXED_POINT
+namespace {
+    constexpr auto use_fixed_point = true;
+    constexpr auto print_stats = true;
+}
+
+namespace {
+    template<typename Scalar>
+    void print_info(mandelbrot::Geometry<Scalar> geometry, int max_iterations) {
+        if (!print_stats) {
+            return;
+        }
+
+        using pack_traits = mandelbrot::pack_traits<Scalar>;
+        std::printf(
+                "set[%d][%d] block[%d][%d] pack[%d] iterations:%d\n",
+                geometry.resolution[1],
+                geometry.resolution[0],
+                pack_traits::block_height,
+                pack_traits::block_width,
+                pack_traits::pack_size,
+                max_iterations);
+    }
+
+    void display(int const *subset_first, mandelbrot::vec2<int> resolution, int max_iterations) {
+        auto const max_displayed_iterations = std::min(max_iterations, 96);
+        for (auto y = 0; y != resolution[1]; ++y) {
+            for (auto x = 0; x != resolution[0]; ++subset_first, ++x) {
+                auto iterations = *subset_first;
+                std::putchar((iterations >= max_displayed_iterations) ? ' ' : ' ' + iterations);
+            }
+            std::putchar('\n');
+        }
+    }
+}
 
 namespace mandelbrot {
     template<typename Rep, int Exponent>
@@ -17,27 +50,13 @@ namespace mandelbrot {
     };
 }
 
-namespace {
-    void display(int const* subset_first, mandelbrot::vec2<int> resolution, int max_iterations) {
-        auto const max_displayed_iterations = std::min(max_iterations, 96);
-        for (auto y = 0; y != resolution[1]; ++y) {
-            for (auto x = 0; x != resolution[0]; ++subset_first, ++x) {
-                auto iterations = *subset_first;
-                std::putchar((iterations >= max_displayed_iterations) ? ' ' : ' ' + iterations);
-            }
-            std::putchar('\n');
-        }
-    }
-}
-
 int main() {
     using namespace mandelbrot;
 
-#if defined(USE_FIXED_POINT)
-    using scalar = sg14::fixed_point<std::int64_t, -28>;
-#else
-    using scalar = float;
-#endif
+    using scalar = std::conditional_t<
+            use_fixed_point,
+            sg14::fixed_point<std::int64_t, -28>,
+            float>;
 
     auto const resolution = vec2<int>{80, 40};
 
@@ -49,6 +68,7 @@ int main() {
     };
     auto const max_iterations = 1000000;
 
+    print_info<>(geometry, max_iterations);
     auto subset = generate<>(geometry, max_iterations);
 
     display(subset.get(), geometry.resolution, max_iterations);
